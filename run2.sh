de8c2b7610c999255149ac02a5c9a9ab4f939a60
Abdulla Fathalla <Fathalla@b-tu.de>
2026-02-11 10:58:39 +0100

    Added run.sh files

---
+++ run2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=SpectralDeepONet
+#SBATCH --output=PointNetMLP_Paper1/GPU2.log
+#SBATCH --error=PointNetMLP_Paper1/GPU2.log
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --cpus-per-task=1
+
+echo "loading modules"
+
+. /home/spack/share/spack/setup-env.sh
+#spack load py-torch
+spack load /j5cepfd
+spack load anaconda3
+
+source /usr1/software/miniconda3/etc/profile.d/conda.sh
+conda activate /usr1/home/abdulla.fathalla/.aixvipmap/envs/MLEnv
+
+echo "starting script"
+
+python -u PointNetMLP_Paper1/SpectralDeepONet/GPU0.py
+
+echo "DONE"
